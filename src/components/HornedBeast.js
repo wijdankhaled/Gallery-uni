0c4772279b0a99fcfe719f25f9fc7b0478b144ef
wijdankhaled <wijdankhaled178@gmail.com>
2021-06-28 15:11:56 +0300

    lab2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 class HornedBeast extends React.Component{
+    constructor (props){
+        super(props);
+        this.state={
+            numofliks:0
+        }
+    }
+
+    increaseOfLikes=()=> {
+        this.setState({
+            numofliks: this.state.numofliks+1
+        })
+    }
     render(){
         return(
-          <div>
-              <h2>
-              {this.props.title}
-              </h2>
-              <image src={this.props.url}/>
-              <p>
-                  {this.props.pra}
-              </p>
-          </div>
+        //   <div>
+        //       <h2>
+        //       {this.props.title}
+        //       </h2>
+        //       <image src={this.props.url}/>
+        //       <p>
+        //           {this.props.pra}
+        //       </p>
+        //   </div>
+        <>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.props.url} />
+  <Card.Body>
+    {/* <Card.Title>{this.props.url}</Card.Title> */}
+    <Card.Text>
+    {this.props.pra}
+    </Card.Text>
+    <Card.Text>
+    ❤️ {this.state.numofliks}
+    </Card.Text>
+    <Button onClick={this.increaseOfLikes}  variant="primary">like </Button>
+  </Card.Body>
+</Card>
+        </>
         )
     }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ class Main extends React.Component{
                 <HornedBeats title={'Rhino Family'} pra={'Mother (or father) rhino with two babies'} url={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'}/>
                 <HornedBeats title={'Unicorn Head'} pra={'Someone wearing a creepy unicorn head mask'}url={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'}/>
             </div>
+            
         )
     }
 }
